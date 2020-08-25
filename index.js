72968fa0b479cddf8f72f1afecab42b259c78585
Razielini <razielco@gmail.com>
2020-08-24 23:20:19 -0600

    Fixing Heroku Error

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ DataBase.connect();
 
 
 // Server
-const server = app.listen(APP_PORT, () => {
+const server = app.listen(APP_PORT, '0.0.0.0', () => {
   console.log(`Listen http://localhost:${server.address().port}`);
 });
