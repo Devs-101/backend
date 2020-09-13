1de95052264046c5c58e1a0467cdf02e363817ce
Razielini <razielco@gmail.com>
2020-09-13 03:31:05 -0600

    Update routes.js

--- api/events/routes.js
+++ api/events/routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 const verifyToken = require('../../utils/verifyToken');
-const { validateEvent } = require('../../utils/verifiedData');
+const { validateEvent, validateEventPublish } = require('../../utils/verifiedData');
 
 const eventsService = require('./service');
 
@@ -13,9 +13,10 @@ function eventRoutes(app, store) {
   router.post('/:organizationId/new', verifyToken, validateEvent, EventsService.registerEvent);
   router.get('/:organizationId/', verifyToken, EventsService.getAllEvents);
   router.get('/:eventId/get', verifyToken, EventsService.getEvent);
+  router.get('/:eventId/get-published', verifyToken, EventsService.getPublishedEvent);
   router.put('/:eventId/update', verifyToken, validateEvent, EventsService.updateEvent);
   router.patch('/:eventId/update', verifyToken, validateEvent, EventsService.updateEvent);
-  router.get('/:eventId/publish', verifyToken, EventsService.publish);
+  router.post('/:eventId/publish', verifyToken, validateEventPublish, EventsService.publish);
   router.get('/:eventId/ready-for-publish', verifyToken, EventsService.readyForPublish);
   router.delete('/:eventId/delete', verifyToken, EventsService.erase);
 }
